963aa20845b1587f089946bd15124129e764d4bd
Douglas Andy Flores Mancilla <daflores@fassil.com.bo>
2022-09-12 14:38:36 -0400

    primer commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Frontend Store</title>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<header class="header">
+    <a href="index.php">
+        <img class="header__logo" src="img/logo.png" alt="Logotipo">
+    </a>
+</header>
+
+<nav class="navegacion">
+    <a class="navegacion__enlace navegacion__enlace--activo" href="#">Tienda</a>
+    <a class="navegacion__enlace" href="#">Nosotros</a>
+</nav>
+
+</body>
+</html>
